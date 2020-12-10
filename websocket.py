8a767d77d490932b6f9d965afb74700cb7a838b3
Max Koren <Koren_Max@bah.com>
2020-12-10 17:27:03 -0500

    hope this works

--- websocket.py
+++ websocket.py
@@ -4,13 +4,11 @@ import random
 import asyncio
 import websockets
 
-from enum import Enum
-from threading import RLock
 from websockets.exceptions import ConnectionClosedError
 
 clients = {}
 messages = []
-goals_left = 1
+goals_left = 35
 
 NAME_PATTERN = re.compile('/([A-Z]{3})')
 
@@ -28,18 +26,20 @@ async def connect(socket, path):
 		if client.online():
 			await socket.close(1002, 'duplicate')
 			return
-		# should this be a method, or am I thinking in Java?
 		client.socket = socket
 		print(f'{name} reconnected')
 	else:
 		client = Client(socket, name)
 		clients[name] = client
 		print(f'{name} connected, {len(clients)} total')
-		
-	await check_unpaired()
 
+	await check_unpaired()
 	await client.handle()
 	print(f'{name} disconnected')
+	
+	
+def online_clients():
+	return client for client in clients.values() if client.online()
 
 
 def new_chat(name):
@@ -49,21 +49,21 @@ def new_chat(name):
 
 
 async def check_unpaired():
-	for client in clients.values():
+	for client in online_clients():
 		if not client.prompt:
 			await client.send_next_prompt()
 
 
-def get_backlog():
-	pass
+def chat_history():
+	return [chat.__dict__ for chat in messages if chat.content]
 
 
 def choose_fair(chooser):
-	active_clients = [client for client in clients.values()
-		if client != chooser and client.online()]
+	active_clients = [client for client in online_clients() if client != chooser]
 	total = len(active_clients)
 	if total == 0:
 		return None
+	# should we update if there's only 1 choice?
 	best = active_clients[0]
 	best_fairness = best.get_fairness()
 	best.update(total)
@@ -78,7 +78,7 @@ def choose_fair(chooser):
 
 
 class Client:
-	
+
 	def __init__(self, socket, name):
 		self.name = name
 		self.socket = socket
@@ -88,41 +88,34 @@ class Client:
 		self.prompts_left = self.codebook.copy()
 		random.shuffle(self.prompts_left)
 
-		self.depending_clients = set()
+		self.contact = None
+		self.cleanup_task = None
 		
 		self.chances = 0
 		self.chosen = 0
 		self.rate = 0
 		
 		self.next_prompt()
-		
-
-	def switch_socket(self, socket):
-		self.socket = socket
 
 
 	async def handle(self):
-		# self.socket = socket
-		# TODO: don't send this if there is no prompt
-		# TODO: send current chat
-		await self.safe_send({
-			'codebook': self.codebook,
-			'goal': goals_left,
-			'prompt': self.prompt
-		})
+		if self.cleanup_task:
+			self.cleanup_task.cancel()
+
+		await self.send_state()
 
 		try:
 			async for message in self.socket:
-				obj = json.loads(message)
-				response = obj.get('response')
+				data = json.loads(message)
+				response = data.get('response')
 				if response:
 					await self.check_response(response)
 					continue
 
 				# TODO: naming!
-				content = obj.get('chat')
+				content = data.get('chat')
 				if content:
-					if obj.get('newline'):
+					if data.get('newline'):
 						self.chat = new_chat(self.name)
 					self.chat.content = content
 					await self.broadcast({'chat': {
@@ -132,21 +125,32 @@ class Client:
 			# this is raised if the socket closes without an error code
 			# in this case, I don't think we should care
 			pass
-		# TODO: update all depending clients
+		self.cleanup_task = asyncio.get_event_loop().create_task(self.cleanup())
+		
+
+	async def send_state(self):
+		await self.safe_send({
+			'codebook': self.codebook,
+			'goal': goals_left,
+			'prompt': self.prompt,
+			'backlog': chat_history(),
+			'myChat': self.chat.content,
+		})
 		
 	
+	def online(self):
+		return not self.socket.closed
+
+
 	async def safe_send(self, message):
 		if self.online():
 			try:
+				# remove Nones?
 				await self.socket.send(json.dumps(message))
 				return True
 			except ConnectionClosedError:
 				pass
 		return False
-		
-	
-	def online(self):
-		return not self.socket.closed
 
 
 	async def broadcast(self, message):
@@ -155,10 +159,10 @@ class Client:
 				await client.safe_send(message)
 
 
-	async def check_response(self, response):
+	async def check_response(self, given_response):
 		global goals_left
-		if response == self.response:
-			self.contact.depending_clients.remove(self)
+		if given_response == self.response:
+			await contact.safe_send({'teamwork': self.name})
 			self.next_prompt()
 			goals_left -= 1
 			await self.safe_send({
@@ -172,11 +176,10 @@ class Client:
 
 	def next_prompt(self):
 		self.contact = choose_fair(self)
-		if self.contact == None:
+		if not self.contact:
 			self.prompt = None
 			self.response = None
 		else:
-			self.contact.depending_clients.add(self)
 			self.prompt, self.response = self.contact.prompts_left.pop()
 			
 	
@@ -184,8 +187,16 @@ class Client:
 		self.next_prompt()
 		if self.prompt:
 			await self.safe_send({'prompt': self.prompt})
-			
-			
+
+
+	async def cleanup(self):
+		# user has 30 seconds to reconnect
+		await asyncio.sleep(30)
+		for client in clients.values():
+			if client.contact == self:
+				await client.send_next_prompt()
+		
+
 	def update(self, choices):
 		# updates the rolling average
 		self.rate = (self.chances * self.rate + 1 / choices) / (self.chances + 1)
@@ -211,9 +222,8 @@ class ChatItem:
 
 
 	def __repr__(self):
-		return f'{self.name}: {"".join(self.content)}'
+		return f'{self.name}: {self.content}'
 
 
-start_server = websockets.serve(connect, port=3637)
-asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_until_complete(websockets.serve(connect, port=3637))
 asyncio.get_event_loop().run_forever()
